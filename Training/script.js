d87df6e13427eb6cb2fd83175627138180509fc5
Thakur <singhshauryapratap753>
2026-05-28 15:40:23 +0530

    for tablet complete responvise task

---
+++ Training/script.js
@@ -0,0 +1,222 @@
+// console.log("jai shree ram")
+
+// let a = "shaurya pratap singh"
+// console.log(a)
+
+// let b = "Rewa"
+// console.log(b)
+
+// let c = "19 years old"
+// console.log(c)
+// console.warn("savdhani rakho")
+// console.error("kuch galat ho gaya")
+// console.table({name:"shaurya", age:19, city:"Rewa", profession:"student", hobby:"coding"})
+// var name = "shaurya"
+// var age = 19
+// var city = "Rewa"
+// var profession = "student"
+// var hobby = "coding"
+// console.log(name, age, city, profession, hobby)
+// var a = 10
+// var b = 20
+// var c = a
+// a = b
+// b = c
+// console.log(a, b)
+// const PI = 3.14
+// console.log(PI)
+
+// var a
+// console.log(a)
+
+// var score = 0
+// score += 10
+// console.log(score)
+
+// var firstName = "shaurya"
+// var middleName = "pratap"
+// var lastName = "singh"
+// var fullname = firstName + " " + middleName + " " + lastName
+// console.log(fullname)
+
+// var a = 10
+// var b = "hello"
+// var c = null
+// var d 
+// var e = true
+// console.log(typeof(a), typeof(b), typeof(c), typeof(d) , typeof(e))
+
+// var a = 10n
+// console.log(typeof(a))
+
+// var a = '50'
+// var b = Number(a)
+// console.log(typeof(b))
+
+// var a = 10
+// var b = String(a)
+// console.log(typeof(b))
+
+// var a = "hello"
+// var b = Boolean(a)
+// console.log(typeof(b))
+
+// var c = "5" + 10
+// console.log(c)
+// var d = "5" - 10
+// console.log(d)
+// var e = "true" + 10
+// console.log(e)
+
+// var a = '123abc'
+// var b = Number(a)
+// console.log(typeof(b))
+
+// var a = parseInt('500px')
+// console.log(a)
+// console.log(typeof(a))
+
+// var a = 10
+// var b = 20
+// var c = a + b
+// console.log(c)
+
+// var a = 10
+// var b = 4
+// console.log(a%b)
+
+// var a = 9
+// console.log(a**2)
+
+// var a = 10
+// a++
+// console.log(a)
+
+// var a = 10
+// a--
+// console.log(a)
+
+// var a = 10
+// var b = 20
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a == b)
+// console.log(a != b)
+// console.log(a >= b)
+// console.log(a <= b)
+
+// var a = "shaurya"
+// var b = 20
+// console.log(a === b)
+// console.log(a == b)
+
+// var a = "shaurya"
+// var b = "shaurya"
+// console.log(a === b)
+// console.log(a == b)
+
+// var a = 'I,am,pratap,  singh'
+// console.log(a.length)
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+// console.log(a.includes('javascript'))
+// console.log(a.slice(6,11))
+// console.log(a.replace('pratap', 'shaurya'))
+// console.log(a.split())
+// console.log(a.remove(6,8))
+
+// var a = Math.round(4.7)
+// console.log(a)
+// var a = Math.floor(4.7)
+// console.log(a)
+// var a = Math.ceil(4.7)
+// console.log(a)
+
+// var a = Math.random(1,10)*10
+// a = Math.floor(a)
+// console.log(a)
+
+// var a = 15
+// a = Math.sqrt(a)
+// console.log(a)
+
+// var a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+// a = Math.max(...a)
+// console.log(a)
+
+// var a = '99.99'
+// a = parseInt(a)
+// console.log(a)
+
+// var a = 3.15448148
+// a = a.toFixed(2)
+// console.log(a)
+
+//  var arr = ['Shuarya', 'RRR ', 'Singh is King', 'KGF', 'Ram Setu', 10, 20, 30, -40, 50.574, false, true, [1, 2, 3]]
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+//  console.log(arr[3])
+//  console.log(arr[12][1])
+
+// var arr = [10, 20, 30, 40, 50]
+// var a = arr[1]
+// arr[1] = arr[3]
+// arr[3] = a
+// console.log(arr)
+
+// var arr = [10, 20, 30]
+// var brr = [40, 50, 60]
+// var crr = [70, 80, 90]
+// var drr = arr[0]+brr[1]+crr[2]
+// var err = arr[2]+brr[1]+crr[0]
+// var frr = arr[0]-brr[1]-crr[2]
+// var grr = arr[0]*brr[1]*crr[2]
+// var hrr = arr[0]/brr[1]/crr[2]
+// console.log(drr)
+// console.log(err)
+// console.log(frr)
+// console.log(grr)
+// console.log(hrr)
+
+// var arr = [15,54, 23, 78, 45, 12, 89, 34, 67, 90]
+// for(var i=0; i<arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         console.log(arr[i], "is even")
+//     }
+//       else{
+//         console.log(arr[i], "is odd")
+//       }
+// }
+
+// var arr = [10,20,30,40,50]
+// var brr = prompt("Enter the number")
+// brr = [brr]
+// brr.forEach(i => {
+//       arr.push(i)
+// });
+// console.log(arr)
+
+// var a = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
+//  while(a.length > 0){
+//     var b = a.pop()
+//     console.log(b)
+//  }
+//  console.log(a)
+
+// var a = ['orange', 'mango', 'apple', 'banana', 'grapes']
+// var b = prompt("Enter the fruit name")
+// for(var i=0; i<5; i++){
+//       a.unshift(i)
+// }
+// a.unshift(b)
+// console.log(a)
+
+// var a = [10,20,30,40,50,60,70,80,90]
+// while( a.length > 0 ) {
+//       let b = a.shift()
+//       console.log(b)
+// }
+// console.log(a)
